f4b75a4b64dae40431deac4a50bda304d962f35c
Nguyen Quoc Dat <forwork.dat2002@gmail.com>
2024-05-25 18:18:30 +0300

    done hw2

--- src/main/java/edu.hsai/autotests/Main.java
+++
@@ -1,7 +0,0 @@
-package edu.hsai.autotests;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/test/java/edu/hsai/hw1/CalculatorTest.java
@@ -0,0 +1,14 @@
+package edu.hsai.hw1;
+
+import com.epam.tat.module4.Calculator;
+import org.junit.jupiter.api.BeforeAll;
+
+public class CalculatorTest {
+    protected static final double DELTA = 0.1;
+    protected static Calculator calculator;
+
+    @BeforeAll
+    static void init() {
+        calculator = new Calculator();
+    }
+}
