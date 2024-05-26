baf01161de3df608cc413f446c42b3c3111438b6
Nguyen Quoc Dat <forwork.dat2002@gmail.com>
2024-05-26 17:49:25 +0300

    done hw4

--- src/test/java/edu/hsai/hw4/DriverSetup.java
+++ src/test/java/edu/hsai/hw4/DriverSetup.java
@@ -1,34 +1,34 @@
-package edu.hsai.hw3;
+package edu.hsai.hw4;
 
-import edu.hsai.hw3.pages.home.HomePage;
+import edu.hsai.hw4.steps.Action;
+import edu.hsai.hw4.steps.Assertion;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
-import java.util.Properties;
-
 class DriverSetup {
     protected static WebDriver driver;
-
-    protected static HomePage homePage;
+    protected static Action action;
+    protected static Assertion assertion;
 
     @BeforeTest
     public void setup() {
-
-        var properties = new Properties();
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         System.setProperty("webdriver.http.factory", "jdk-http-client");
         Dotenv dotenv = Dotenv.configure().load();
 
-
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        action = new Action(driver, dotenv);
+        assertion = new Assertion(driver, dotenv);
 
-        homePage = new HomePage(driver, dotenv.get("url"));
-        // Perform login
-        homePage.performLogin(dotenv.get("name"), dotenv.get("password"));
+        // 1. Open test site by URL
+        action.navigateToHomePage();
+
+        // 3. Perform login
+        action.performLogin();
     }
 
     @AfterTest
